cab8d95e28a6ca314b91f2b1378e7e1105416018
Tamir Yamaleyev <tamirka8@gmail.com>
2025-09-27 18:05:14 +0300

    Added simple chasing Enemy AI and top down camera script

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ public class EnemySpawner : MonoBehaviour
     public string enemyTag = "Enemy";
     public float spawnInterval = 2f;
     private float timer;
+    public float spawnHeight = 1f;
 
     void Update()
     {
@@ -12,7 +13,7 @@ public class EnemySpawner : MonoBehaviour
         if (timer > spawnInterval)
         {
             timer = 0f;
-            Vector3 randomPos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+            Vector3 randomPos = new Vector3(Random.Range(-10, 10), spawnHeight, Random.Range(-10, 10));
             ObjectPooler.Instance.SpawnFromPool(enemyTag, randomPos, Quaternion.identity);
         }
     }
